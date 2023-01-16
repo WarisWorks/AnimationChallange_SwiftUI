5cd3d3ad8794a395ff9fab8030157a75aa00b418
Waris Works <warisruzi@gmail.com>
2023-01-16 16:55:21 +0900

    SwiftUI -> Animation Challange

---
+++ AnimationChallange/AnimationChallangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationChallangeApp.swift
+//  AnimationChallange
+//
+//  Created by Waris on 2023/01/16.
+//
+
+import SwiftUI
+
+@main
+struct AnimationChallangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationChallange/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimationChallange
+//
+//  Created by Waris on 2023/01/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Home()
+            .preferredColorScheme(.light)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
